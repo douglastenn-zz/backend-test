8316e2b832b8d51d2e9ebb7393e9cb61669fc3de
Douglas Tenório <douglas.develop@gmail.com>
2016-08-08 04:16:24 -0300

    Aplicação de filtro simples por similaridade

--- app/Http/Controllers/VagaController.php
+++ app/Http/Controllers/VagaController.php
@@ -11,7 +11,8 @@ class VagaController extends Controller
      */
     public function __construct()
     {
-      //
+      $data = file_get_contents(base_path().'/vagas.json');
+      $this->json = json_decode($data, true);
     }
 
     /**
@@ -21,9 +22,25 @@ class VagaController extends Controller
      */
     public function findAll()
     {
-      echo base_path().'/vagas,json';
-      $data = file_get_contents(base_path().'/vagas,json');
-      $json = json_decode($data, true);
-      echo 'xxxxxxxxxxxxx'; exit  ;
+
+      $value = 'Gaspar';
+      $type = 'cidade';
+      print_r($this->filter($value, $type));
+      // print_r($filtred); exit;
+      // return response()->json($this->json);
+    }
+
+    public function filter($value, $type)
+    {
+      $filtred = [];
+      foreach($this->json['docs'] as $vaga) {
+        // Verifica a similaridade dos textos e retorna uma porcentagem na variável $percent
+        similar_text($vaga[$type][0], $value, $percent);
+        // Adiciona vagas com o texto que tenha mais que 70% de similaridade
+        if($percent >= 70) {
+          array_push($filtred, $vaga);
+        }
+      }
+      return $filtred;
     }
 }
